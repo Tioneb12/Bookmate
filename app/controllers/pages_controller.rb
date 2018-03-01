bdd67796e510da8df41397de742619e7466f2426
DamienBoussardon <Damien.boussardon@gmail.com>
2018-03-01 14:26:12 +0100

    modif pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,9 @@ class PagesController < ApplicationController
 
   def home
     @books = Book.all
+    user_id = current_user
+    @readings = Reading.where(["user_id = :user_id", {user_id: user_id}]).select(:book_id)
+
   end
 
 end
