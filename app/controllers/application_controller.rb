df3ccea567ebe9fc77149ae67116928dcc27f6c5
Joseph Binyoum Bayemi <b.bj@hotmail.fr>
2018-03-01 14:25:45 +0100

    create method in appcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,25 @@
-class ApplicationController < ActionController::Base
+  class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :create_session_books
+
+  def create_session_books
+    # si le tableau de session books existe pas, le creer
+    session[:books] = [] if session[:books].nil?
+  end
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :avatar, :bio, :address])
   end
 
+  Warden::Manager.after_authentication do |user,auth,opts|
+    # for each google book id in session books
+      # check if book not exist
+        # api request + create book
+      # create reading for current user
+    # empty sesion books
+  end
 end
