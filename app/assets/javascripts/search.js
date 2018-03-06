ee9b1ea4442dbc582b7c9dcc7b902adb02038b6c
Joseph Binyoum Bayemi <b.bj@hotmail.fr>
2018-03-06 19:12:51 +0100

    remove big arrow

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,6 +1,6 @@
 var form = document.querySelector(".simple_form.search");
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", function() {
   var results_book = document.getElementById("book-results");
   // vider le results_book
   results_book.innerHTML = "";
