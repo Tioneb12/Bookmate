8bac2d71a206f800dedf8d16211fb208148cd8ff
DamienBoussardon <Damien.boussardon@gmail.com>
2018-03-06 15:06:10 +0100

    working chat without WS

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -4,14 +4,12 @@ class ConversationsController < ApplicationController
     @conversation = Conversation.new(recipient: current_user, sender_id: params[:user_id])
     @conversation.save
     redirect_to conversation_path(@conversation)
-    # create a Conversation between curent_user and user found in routes
-    # redirect to this conversation show page
   end
 
   def show
-    # @messages = Message.all
     @message = Message.new
     @messages = Message.all
     @conversation = Conversation.find(params[:id])
+    @user = @conversation.opposed_user(current_user)
   end
 end
