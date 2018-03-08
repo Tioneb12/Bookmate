259bd6b97d10e36e2936160c50407609a57ab280
Joseph Binyoum Bayemi <b.bj@hotmail.fr>
2018-03-08 16:59:37 +0100

    remove es6 big arrow

--- app/assets/javascripts/no-sign.js
+++ app/assets/javascripts/no-sign.js
@@ -1,7 +1,7 @@
  var form = document.getElementById("display-form");
   var cacher = document.querySelector(".explication");
   var visible = document.querySelector(".form-inscription")
-  form.addEventListener("click", (even) => {
+  form.addEventListener("click", function() {
     cacher.classList.add("no-display");
     visible.classList.remove("no-display");
     visible.classList.remove("display");
